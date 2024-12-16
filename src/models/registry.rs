9a8413340eb255f1f5f87d32c614bea78bdbe4d3
xiebiao <xiebiao360@outlook.com>
2024-12-16 16:13:51 +0800

    添加注册表主机字段，重构 Registry 结构体以支持可选字段，更新创建注册表方法以适应新结构，优化默认值设置

--- src/models/registry.rs
+++ src/models/registry.rs
@@ -1,4 +1,5 @@
 use anyhow::Result;
+use chrono::Utc;
 use sqlx::FromRow;
 
 use crate::web_server::AppState;
@@ -7,14 +8,45 @@ use crate::web_server::AppState;
 pub struct Registry {
     pub id: i64,
     pub name: String,
+    pub host: String,
     pub port: i64,
-    pub username: String,
-    pub password: String,
-    pub cert_file: String,
-    pub key_file: String,
-    pub status: String,
+    pub username: Option<String>,
+    pub password: Option<String>,
+    pub cert_file: Option<String>,
+    pub key_file: Option<String>,
+    pub status: Option<String>,
     pub created_at: i64,
     pub updated_at: i64,
+}
+
+impl Registry {
+    pub fn new(name: String, host: String, port: i64) -> Self {
+        Self {
+            id: 0,
+            name,
+            host,
+            port,
+            ..Default::default()
+        }
+    }
+}
+
+impl Default for Registry {
+    fn default() -> Self {
+        Self {
+            id: 0,
+            name: "".to_string(),
+            host: "".to_string(),
+            port: 0,
+            username: None,
+            password: None,
+            cert_file: None,
+            key_file: None,
+            status: None,
+            created_at: Utc::now().timestamp(),
+            updated_at: Utc::now().timestamp(),
+        }
+    }
 }
 
 impl AppState {
@@ -33,28 +65,24 @@ impl AppState {
         Ok(registry)
     }
 
-    pub async fn create_registry(
-        &self,
-        name: &str,
-        port: i32,
-        username: &str,
-        password: &str,
-        cert_file: &str,
-        key_file: &str,
-    ) -> Result<()> {
-        sqlx::query(
-            r#"INSERT INTO registry (name, port, username, password, cert_file, key_file)
-            VALUES ($1, $2, $3, $4, $5, $6)"#,
+    pub async fn create_registry(&self, registry: &Registry) -> Result<Registry> {
+        let registry = sqlx::query_as(
+            r#"INSERT INTO registry (name, host, port, username, password, cert_file, key_file, status, created_at, updated_at)
+            VALUES ($1, $2, $3, $4, $5, $6, $7, $8, $9, $10) RETURNING *"#,
         )
-        .bind(name)
-        .bind(port)
-        .bind(username)
-        .bind(password)
-        .bind(cert_file)
-        .bind(key_file)
-        .execute(&self.pool)
+        .bind(&registry.name)
+        .bind(&registry.host)
+        .bind(registry.port)
+        .bind(&registry.username)
+        .bind(&registry.password)
+        .bind(&registry.cert_file)
+        .bind(&registry.key_file)
+        .bind(&registry.status)
+        .bind(registry.created_at)
+        .bind(registry.updated_at)
+        .fetch_one(&self.pool)
         .await?;
-        Ok(())
+        Ok(registry)
     }
 
     pub async fn delete_registry(&self, id: i64) -> Result<()> {
