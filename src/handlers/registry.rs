d45c0a168a38b3c6b5e3db1c352af1ff20a585b0
xiebiao <xiebiao360@outlook.com>
2025-01-17 20:30:33 +0800

    移除未使用的 SSL 处理逻辑，简化注册表创建处理程序

--- src/handlers/registry.rs
+++ src/handlers/registry.rs
@@ -1,11 +1,6 @@
 use axum::{extract::State, response::IntoResponse, Json};
 
-use crate::{
-    error::AppError,
-    models::registry::{CreateRegistryInput, SslType},
-    utils::generate_self_signed_file,
-    web_server::AppState,
-};
+use crate::{error::AppError, models::registry::CreateRegistryInput, web_server::AppState};
 
 pub async fn create_registry_handler(
     State(state): State<AppState>,
@@ -25,18 +20,18 @@ pub async fn create_registry_handler(
         )));
     }
 
-    if let Some(ssl) = input.ssl {
-        let (cert_file, key_file) = match ssl {
-            SslType::SelfSigned(ss) => {
-                generate_self_signed_file(&ss.cert_dir).await?;
-                (
-                    format!("{}/registry.crt", &ss.cert_dir),
-                    format!("{}/registry.key", &ss.cert_dir),
-                )
-            }
-            SslType::Provided(p) => (p.cert_file, p.key_file),
-        };
-    }
+    // if let Some(ssl) = input.ssl {
+    //     let (cert_file, key_file) = match ssl {
+    //         SslType::SelfSigned(ss) => {
+    //             generate_self_signed_file(&ss.cert_dir).await?;
+    //             (
+    //                 format!("{}/registry.crt", &ss.cert_dir),
+    //                 format!("{}/registry.key", &ss.cert_dir),
+    //             )
+    //         }
+    //         SslType::Provided(p) => (p.cert_file, p.key_file),
+    //     };
+    // }
 
     Ok(())
 }
