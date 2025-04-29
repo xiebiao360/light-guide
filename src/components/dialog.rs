22cc228eb6fecade58e523ff6c0ef39e97db680b
xiebiao <xiebiao360@outlook.com>
2025-04-29 10:16:13 +0800

    优化对话框组件的样式和逻辑，调整背景透明度，增强过渡效果

--- src/components/dialog.rs
+++ src/components/dialog.rs
@@ -9,16 +9,30 @@ pub struct DialogProps {
 
 #[component]
 pub fn Dialog(props: DialogProps) -> Element {
+    let overlay_class = use_signal(|| String::from("dialog-overlay"));
+    let content_class = use_signal(|| String::from("dialog-content"));
+
+    use_effect(move || {
+        to_owned![overlay_class, content_class];
+        if props.show {
+            overlay_class.set("dialog-overlay show".to_string());
+            content_class.set("dialog-content show".to_string());
+        } else {
+            overlay_class.set("dialog-overlay".to_string());
+            content_class.set("dialog-content".to_string());
+        }
+    });
+
     if !props.show {
         rsx! {}
     } else {
         rsx! {
             div {
-                class: "dialog-overlay",
+                class: overlay_class.read().to_string(),
                 onclick: move |_| props.on_close.call(()),
 
                 div {
-                    class: "dialog-content",
+                    class: content_class.read().to_string(),
                     onclick: |e| e.stop_propagation(),
 
                     {props.children}
