922960daf4bf97587c39f5f7cdb04ad09e70d9a4
xiebiao <xiebiao360@outlook.com>
2025-05-22 07:08:05 +0800

    feat: initialize Vue 3 project with Vite, add basic structure and components
    - Add favicon.ico for the application.
    - Create main application file (App.vue) with header and navigation.
    - Implement base CSS styles for the application.
    - Add logo.svg for branding.
    - Create main CSS file for layout and styling.
    - Develop HelloWorld component to display a welcome message.
    - Create TheWelcome component with multiple WelcomeItem components for documentation and support.
    - Implement WelcomeItem component for displaying individual items in TheWelcome.
    - Add various icon components for visual representation.
    - Set up main.js to initialize Vue app with Pinia and router.
    - Configure router with Home and About views.
    - Create counter store using Pinia for state management.
    - Develop AboutView and HomeView components for routing.
    - Set up Vite configuration for development and build processes.

--- src/main.rs
+++ src/main.rs
@@ -1,16 +1,23 @@
 use anyhow::Result;
-use light_guide::AppState;
+use light_guide::{AppState, load_router};
+use tokio::net::TcpListener;
 use tracing::{info, level_filters::LevelFilter};
-use tracing_subscriber::{fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt, Layer as _};
+use tracing_subscriber::{Layer as _, fmt::Layer, layer::SubscriberExt, util::SubscriberInitExt};
 
 #[tokio::main]
 async fn main() -> Result<()> {
     let layer = Layer::new().with_filter(LevelFilter::INFO);
     tracing_subscriber::registry().with(layer).init();
 
-    let db_path = "sqlite://test.db";
+    let db_path = "sqlite://guide.db";
     let state = AppState::try_new(db_path).await?;
-    info!("Database connection established");
+    let app = load_router(state).await?;
+    let addr: &str = "127.0.0.1:3000";
+    let listener = TcpListener::bind(addr).await?;
+
+    info!("Listening on {}", addr);
+
+    axum::serve(listener, app).await?;
 
     Ok(())
 }
---
+++ web/vite.config.js
@@ -0,0 +1,58 @@
+import { fileURLToPath, URL } from 'node:url'
+
+import { defineConfig } from 'vite'
+import vue from '@vitejs/plugin-vue'
+import vueJsx from '@vitejs/plugin-vue-jsx'
+import vueDevTools from 'vite-plugin-vue-devtools'
+
+// https://vite.dev/config/
+export default defineConfig({
+  base: '/web/',
+  server: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': {
+        target: 'http://localhost:8080',
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, '')
+      }
+    }
+  },
+  css: {
+    preprocessorOptions: {
+      scss: {
+        additionalData: '@import "@/assets/scss/variables.scss";'
+      }
+    }
+  },
+  define: {
+    'process.env': {}
+  },
+  optimizeDeps: {
+    include: ['vue', 'vue-router', 'vuex', 'axios']
+  },
+  build: {
+    outDir: '../dist',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return id.toString().split('node_modules/')[1].split('/')[0].toString()
+          }
+        }
+      }
+    }
+  },
+  plugins: [
+    vue(),
+    vueJsx(),
+    vueDevTools(),
+  ],
+  resolve: {
+    alias: {
+      '@': fileURLToPath(new URL('./src', import.meta.url))
+    },
+  },
+})
